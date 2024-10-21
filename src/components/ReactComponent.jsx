7ce00c99a7ada3459263e87c0d4f964a5d7ef3ce
joacomesa22 <joacomesa@gmail.com>
2024-10-21 18:38:31 -0400

    added react component

---
+++ src/components/ReactComponent.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReactComponent = () => {
+  return (
+    <div className="py-40 text-center font-bold text-5xl text-purple-400">
+      <h1>Hello, React!</h1>
+    </div>
+  );
+};
+
+export default ReactComponent;
